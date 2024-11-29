327b4e43944698c508857497a4f83722292d3b19
Keerthana5027 <skeerthana480@gmail.com>
2024-11-29 12:34:16 +0530

    logo_change

--- my-app/src/components/header.js
+++ my-app/src/components/header.js
@@ -7,8 +7,8 @@ const Header =() =>{
     <>
          {/* <header>Cities</header> */}
          <header class="header"> 
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJc-kxhH7hFPOh4cDOLyfk9oMeGhBclA0pog&s" alt="Company Logo" class="logo"/> 
-            <h1 class="company-name">Company Name</h1> 
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJMERUJSyUthYAXV-g80Y--KOkGomFexwj0w&s" alt="Company Logo" class="logo"/> 
+            <h1 class="company-name">Evanto</h1> 
             <div class="auth-buttons"> 
                 <Link to="sign-in"><button class="btn">sign In</button></Link>
                 <Link to="sign-out"><button class="btn">sign Out</button></Link>
